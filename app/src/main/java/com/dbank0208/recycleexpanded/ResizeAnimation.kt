b88fee2f2646734058e1bef4e310cd2160628e2e
daisukenagata <dbank0208@gmail.com>
2019-04-03 08:31:14 +0900

    null tolerance

--- app/src/main/java/com/dbank0208/recycleexpanded/ResizeAnimation.kt
+++ app/src/main/java/com/dbank0208/recycleexpanded/ResizeAnimation.kt
@@ -8,7 +8,7 @@ class ResizeAnimation(view: View,addHeight: Int,startHeight: Int) : Animation() 
 
     private  var addHeight  = 0
     private  var startHeight = 0
-    lateinit var view: View
+    private  var view: View? = null
     
     init {
         this.addHeight = addHeight
@@ -18,8 +18,8 @@ class ResizeAnimation(view: View,addHeight: Int,startHeight: Int) : Animation() 
 
     override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
         val newHeight = (startHeight + addHeight * interpolatedTime).toInt()
-        view.getLayoutParams().height = newHeight
-        view.requestLayout()
+        view?.getLayoutParams()?.height = newHeight
+        view?.requestLayout()
     }
 
     override fun willChangeBounds(): Boolean { return true }
